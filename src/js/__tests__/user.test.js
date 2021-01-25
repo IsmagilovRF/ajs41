204d290cb128d2955f1d4bec22e0608e4a2b1926
IsmagilovRF <ledohod@icloud.com>
2021-01-25 21:08:23 +0300

    initial commit

--- src/js/__tests__/user.test.js
+++ src/js/__tests__/user.test.js
@@ -7,10 +7,13 @@ beforeEach(() => {
   jest.resetAllMocks();
 });
 
+// httpGet.mockReturnValue(JSON.stringify({}));
 test('should call loadUser once', () => {
-  httpGet.mockReturnValue(JSON.stringify({}));
+  httpGet.mockReturnValue('{}');
 
   const response = loadUser(1);
   expect(response).toEqual({});
   expect(httpGet).toBeCalledWith('http://server:8080/users/1');
 });
+
+test('kjlkjljlk', () => { });
--- webpack.config.js
+++ webpack.config.js
@@ -42,4 +42,10 @@ module.exports = {
       chunkFilename: '[id].css',
     }),
   ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 8080,
+    open: true,
+  },
 };
