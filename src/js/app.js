32c4ef65c2dab5f6e95c92cc90a57cf6cd919740
IsmagilovRF <ledohod@icloud.com>
2021-01-27 00:03:16 +0300

    initial commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,13 @@
 // TODO: write your code here
-import sum from './basic';
+export default function characterStatus(character) {
+  let status = '';
 
-console.log('worked');
-
-console.log(sum([1, 2]));
+  if (character.health > 50) {
+    status = 'healthy';
+  } else if (character.health > 14 && character.health < 51) {
+    status = 'wounded';
+  } else if (character.health < 15) {
+    status = 'critical';
+  }
+  return status;
+}
